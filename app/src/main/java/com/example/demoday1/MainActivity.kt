ce2cf0a9d488ee42f1babd4289d454f95b0ebc45
VoodooPDX <ourquotesdefineus@gmail.com>
2019-08-05 10:20:00 -0700

    Test

---
+++ app/src/main/java/com/example/demoday1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demoday1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
